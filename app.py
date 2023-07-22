d2f7b6b53ef8795ca0d6f7c027cbb77bce289286
Alice V <ooisee@gmail.com>
2023-07-22 18:43:07 +0100

    2 llms

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ import requests
 import json
 from langchain.schema import SystemMessage
 from fastapi import FastAPI
+import streamlit as st
 
 load_dotenv()
 brwoserless_api_key = os.getenv("BROWSERLESS_API_KEY")
@@ -84,24 +85,42 @@ def scrape_website(objective: str, url: str):
 
 
 def summary(objective, content):
-    llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-16k-0613")
+    llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-16k")
 
     text_splitter = RecursiveCharacterTextSplitter(
         separators=["\n\n", "\n"], chunk_size=10000, chunk_overlap=500)
     docs = text_splitter.create_documents([content])
     map_prompt = """
-    Write a summary of the following text for {objective}:
-    "{text}"
+    Write a verbose and detailed summary for "[{objective}]", following by short list of key points as bullets, and source urls, max 8000 tokens, based on the following content:
+    ### Start of content:
+
+    
+    {text}
+
+    
+    ### End of content
+    SUMMARY:
+    """
+    combine_prompt = """
+    Write a verbose and detailed reasearch paper for "[{objective}]", following by short list of key points as bullets, and source urls, max 12000 tokens, based on the following content:
+    ### Start of content:
+
+    
+    {text}
+
+    
+    ### End of content
     SUMMARY:
     """
     map_prompt_template = PromptTemplate(
         template=map_prompt, input_variables=["text", "objective"])
-
+    combine_prompt_template = PromptTemplate(
+        template=combine_prompt, input_variables=["text", "objective"] )
     summary_chain = load_summarize_chain(
         llm=llm,
         chain_type='map_reduce',
         map_prompt=map_prompt_template,
-        combine_prompt=map_prompt_template,
+        combine_prompt=combine_prompt_template,
         verbose=True
     )
 
@@ -113,13 +132,13 @@ def summary(objective, content):
 class ScrapeWebsiteInput(BaseModel):
     """Inputs for scrape_website"""
     objective: str = Field(
-        description="The objective & task that users give to the agent")
-    url: str = Field(description="The url of the website to be scraped")
+        description="The objective & task that users give to the agent.")
+    url: str = Field(description="The url of the website to be scraped.")
 
 
 class ScrapeWebsiteTool(BaseTool):
     name = "scrape_website"
-    description = "useful when you need to get data from a website url, passing both url and objective to the function; DO NOT make up any url, the url should only be from the search results"
+    description = "Useful when you need to get data from a website url, passing both url and objective to the function; DO NOT make up any url, the url should only be from the search results."
     args_schema: Type[BaseModel] = ScrapeWebsiteInput
 
     def _run(self, objective: str, url: str):
@@ -134,22 +153,26 @@ tools = [
     Tool(
         name="Search",
         func=search,
-        description="useful for when you need to answer questions about current events, data. You should ask targeted questions"
+        description="Useful for when you need to answer questions about current events, data. You should ask targeted questions."
     ),
     ScrapeWebsiteTool(),
 ]
 
 system_message = SystemMessage(
-    content="""You are a world class researcher, who can do detailed research on any topic and produce facts based results; 
-            you do not make things up, you will try as hard as possible to gather facts & data to back up the research
+    content="""You are a world class researcher, who can do detailed research on any topic and produce facts based results.
+            You do not make things up, you will try as hard as possible to gather facts & data to back up the research.
             
             Please make sure you complete the objective above with the following rules:
-            1/ You should do enough research to gather as much information as possible about the objective
-            2/ If there are url of relevant links & articles, you will scrape it to gather more information
-            3/ After scraping & search, you should think "is there any new things i should search & scraping based on the data I collected to increase research quality?" If answer is yes, continue; But don't do this more than 3 iteratins
-            4/ You should not make things up, you should only write facts & data that you have gathered
-            5/ In the final output, You should include all reference data & links to back up your research; You should include all reference data & links to back up your research
-            6/ In the final output, You should include all reference data & links to back up your research; You should include all reference data & links to back up your research"""
+            1. You should do enough research to gather as much information as possible about the objective.
+            2. If there are url of relevant links & articles, you will scrape it to gather more information.
+            3. After scraping & search, you should think "is there any new things I should search & scraping based on the data I collected to increase research quality?" If answer is yes, continue; Do from 5 to 10 iterations.
+            4. Reflect on result.
+            5. You should not make things up, you should only write facts & data that you have gathered. 
+            6. Try to at least first 10 links of the search results, and scrape the content of the website to gather more information.
+            7. You should not write anything that is not related to the objective.
+            8. In the final output, You should include all reference data & links to back up your research; You should include all reference data & links to back up your research.
+            9. In the final output, You should include all reference data & links to back up your research; You should include all reference data & links to back up your research.
+            """
 )
 
 agent_kwargs = {
@@ -157,50 +180,65 @@ agent_kwargs = {
     "system_message": system_message,
 }
 
-llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-16k-0613")
+llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-16k")
+llm2 = ChatOpenAI(temperature=0.7, model="gpt-3.5-turbo-16k")   
 memory = ConversationSummaryBufferMemory(
-    memory_key="memory", return_messages=True, llm=llm, max_token_limit=1000)
+    memory_key="memory", return_messages=True, llm=llm, max_token_limit=12000)
 
-agent = initialize_agent(
-    tools,
-    llm,
-    agent=AgentType.OPENAI_FUNCTIONS,
-    verbose=True,
-    agent_kwargs=agent_kwargs,
-    memory=memory,
-)
+def bake_agent(llm):
+    agent = initialize_agent(
+        tools,
+        llm,
+        agent=AgentType.OPENAI_FUNCTIONS,
+        verbose=True,
+        agent_kwargs=agent_kwargs,
+        memory=memory,
+    )
+    return agent
 
 
 # 4. Use streamlit to create a web app
-# def main():
-#     st.set_page_config(page_title="AI research agent", page_icon=":bird:")
+def main():
+    st.set_page_config(page_title="AI research agent", page_icon=":bird:")
 
-#     st.header("AI research agent :bird:")
-#     query = st.text_input("Research goal")
+    st.header("AI research agent :bird:")
+    query = st.text_input("Research goal")
 
-#     if query:
-#         st.write("Doing research for ", query)
+    if query:
+        st.write("Doing research for ", query)
 
-#         result = agent({"input": query})
+        agent = bake_agent(llm)
+        agent_2 = bake_agent(llm2)
+        result = agent({"input": query})
 
-#         st.info(result['output'])
+        st.info(result['output'])
+        
+        result_2 = agent_2({"input": query}) 
+
+        st.info(result_2['output'])
+
+        summ = summary(query, result['output'] + result_2['output'])
+
+        st.info(summ)
+
+        
 
 
-# if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+    main()
 
 
-# 5. Set this as an API endpoint via FastAPI
-app = FastAPI()
+# # 5. Set this as an API endpoint via FastAPI
+# app = FastAPI()
 
 
-class Query(BaseModel):
-    query: str
+# class Query(BaseModel):
+#     query: str
 
 
-@app.post("/")
-def researchAgent(query: Query):
-    query = query.query
-    content = agent({"input": query})
-    actual_content = content['output']
-    return actual_content
+# @app.post("/")
+# def researchAgent(query: Query):
+#     query = query.query
+#     content = agent({"input": query})
+#     actual_content = content['output']
+#     return actual_content
